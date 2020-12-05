52e0e93c25d16636b7734de326c333d996e1affc
erinszabo <55733370+erinszabo@users.noreply.github.com>
2020-12-04 17:12:29 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+Chapter 7 in Runestone Exercises:
+# 1-4
+"""
+
+if __name__ == '__main__':
+    print("")
+    print('1) Extend the buildParseTree function to handle mathematical expressions that do not have spaces between '
+          'every character.')
+
+    print("")
+    print('2) Modify the buildParseTree and evaluate functions to handle boolean statements (and, or, and not). '
+          'Remember that “not” is a unary operator, so this will complicate your code somewhat.')
+
+    print("")
+    print('3) Using the findSuccessor method, write a non-recursive inorder traversal for a binary search tree.')
+
+    print("")
+    print('4) Modify the code for a binary search tree to make it threaded. Write a non-recursive inorder traversal '
+          'method for the threaded binary search tree. A threaded binary tree maintains a reference from each node to '
+          'its successor.')
+
+    print("")
